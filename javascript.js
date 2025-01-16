af403f59bde3174fdb37854ceeae972b80353cce
jennyhavens <jennifer.havens91@gmail.com>
2025-01-15 18:33:07 -0600

    Write basic calculation functions that work in console

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const add = function(a, b) {
+    return (a + b);
+  };
+  console.log(add(39, 1));
+  
+  const subtract = function(a, b) {
+    return (a - b);
+  };
+  console.log(subtract(41, 1));
+  
+  const multiply = function(a, b) {
+    return (a * b);
+  };
+  console.log(multiply(8, 5));
+
+  const divide = function(a, b) {
+    return (a / b);
+  }
+  console.log(divide(80, 2));
