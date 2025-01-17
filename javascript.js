f603fe02d4c31129bd1543a255adcd47be694ed9
jennyhavens <jennifer.havens91@gmail.com>
2025-01-16 18:37:44 -0600

    Start event listeners

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-let num1 = parseFloat(prompt("Enter first number"));
-let operator = prompt("Enter operator");
-let num2 = parseFloat(prompt("Enter second number"));
-let result;
+//let num1 = parseFloat(prompt("Enter first number"));
+//let operator = prompt("Enter operator");
+//let num2 = parseFloat(prompt("Enter second number"));
+//let result;
 
+function popDisplay() {
+    const populate = document.querySelector('.largeButtons');
+    populate.addEventListener('click', popDisplay);
+    alert ('it was clicked!');
+};
+
+popDisplay();
 
 const add = function(num1, num2) {
     return (num1 + num2);
